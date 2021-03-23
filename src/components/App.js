4d4b2bd2b7521703b66b3af9bc3bc80f6bdc4b7d
o0p <rus@gmx.com>
2021-03-23 23:34:09 +0000

    Add ImageList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const res = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 7wNJ_4HIGqQrt4UlFj7uxVlkSOkCcUTjPobwwk9kqMI',
+      },
+    });
+    console.log(res.data.results);
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList />
       </div>
     );
   }
